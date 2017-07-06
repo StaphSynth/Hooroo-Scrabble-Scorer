7b7df77eddcca518d9fe190ed5e56b2e35a3028c
Staphsynth <1337dallen@gmail.com>
2017-07-06 17:35:10 +1000

    refactor score to put computational and bonus deciding logic into a private method

--- scrabble.rb
+++ scrabble.rb
@@ -10,15 +10,23 @@ class Scrabble
       'U' => 1, 'V' => 4, 'W' => 4, 'X' => 8,
       'Y' => 4, 'Z' => 10
     }
+
+    @@bonus_multipliers = {
+      n: 1, dw: 2,
+      tw: 3, dl: 2,
+      tl: 3
+    }
+
+    @total_score = 0
   end
 
   #returns the word score. Takes arg word: string containing only alpha characters.
   #arg bonus: array of length == word, descibing any special tiles the letters occupy.
   #Symobols used: :dw = double word score, :tw = tripple word score
   #               :dl = double letter score, :tl = tripple letter score
-  #               nil = no bonus on that tile
+  #               :n = no bonus on that tile
   #
-  #EG: [nil, :dw, 'nil', nil], [nil, :tl, nil, :dw, nil, nil, nil], etc
+  #EG: [:n, :dw, :n, :n], [:n, :tl, :n, :dw, :n, :n, :n], etc
   def score(word, bonus = nil)
     if(!word || word.empty?)
       return 0
@@ -34,14 +42,25 @@ class Scrabble
       elsif(bonus.length != word.length)
         raise ArgumentError, 'Bonus array.length must equal word.length'
       end
+    else
+      bonus = Array.new(word.length)
     end
 
-    score = 0
-
-    word.chars.each do |char|
-      score += @@letter_scores[char.upcase]
-    end
-
+    score = compute_score(word, bonus)
+    @total_score += score
     return score
   end
+
+  private
+    def compute_score(word, bonus)
+      score = 0
+
+      word = word.chars.zip(bonus)
+      word.each do |char, bonus|
+        score += @@letter_scores[char.upcase]
+      end
+      return score
+    end
 end
+
+puts Scrabble.new.score("oxidizes")
